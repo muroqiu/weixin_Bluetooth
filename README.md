679f87a52be47a1bad229369f38ac7d15df51030
muroqiu <muroqiu@qq.com>
2018-03-08 20:15:22 +0800

    完善注释等

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # weixin_Bluetooth
 微信蓝牙测试
+
+#### 简单介绍下流程：
+* 初始化小程序蓝牙模块 openBluetoothAdapter；
+* 获取手机系统已连接的蓝牙设备 getConnectedBluetoothDevices；
+* 建立BLE连接 createBLEConnection；
+* 获取BLE服务的UUID及相关特征值Characteristics的UUID，设置设备数据变化监听；
+* 向蓝牙设备发送测试数据 writeBLECharacteristicValue；
--- pages/index/index.js
+++ pages/index/index.js
@@ -6,8 +6,9 @@ const SERVICE_UUID = "91680001-1111-6666-8888-0123456789AB"
 const WRITE_UUID = "91680002-1111-6666-8888-0123456789AB"
 const NOTIFY_UUID = "91680003-1111-6666-8888-0123456789AB"
 
-// var mDeviceID = 'FF:22:F8:CB:68:F9'
-var mDeviceID = "7ECAADA5-3336-1B67-069B-287DF71EAF91"
+// 
+var mDeviceID = 'FF:22:F8:CB:68:F9'
+// var mDeviceID = "7ECAADA5-3336-1B67-069B-287DF71EAF91"
 
 Page({
   data: {
@@ -25,9 +26,9 @@ Page({
   },
 
   /**
-   * 打开系统蓝牙
+   * 初始化小程序蓝牙模块
    */
-  openBluetooth: function () {
+  openBluetoothAdapter: function () {
     var that = this;
     wx.openBluetoothAdapter({
       success: function (res) {
@@ -37,22 +38,16 @@ Page({
         })
         // 监听蓝牙适配器状态变化
         wx.onBluetoothAdapterStateChange(function (res) {
-          // console.log(res)
-          that.setData({
-            // debugMsg: res.errMsg
-          })
+          console.log(res)
         })
         // 发现新设备
         wx.onBluetoothDeviceFound(function (devices) {
           console.log(devices)
-          that.setData({
-            // debugMsg: devices[0].advertisServiceUUIDss
-          })
         })
       },
 
       fail: function (res) {
-        // console.log(res)
+        console.log(res)
         that.setData({
           debugMsg: res.errMsg
         })
@@ -61,37 +56,7 @@ Page({
   },
 
   /**
-   * 搜索蓝牙设备
-   */
-  startBluetoothDevicesDiscovery: function (e) {
-    var that = this;
-    // 以微信硬件平台的蓝牙智能灯为例，主服务的 UUID 是 FEE7。传入这个参数，只搜索主服务 UUID 为 FEE7 的设备
-    wx.startBluetoothDevicesDiscovery({
-      services: [SERVICE_UUID],
-      success: function (res) {
-        console.log(res)
-      },
-      fail: function (res) {
-        console.log(res)
-        that.setData({
-          debugMsg: res.errMsg
-        })
-      },
-    })
-  },
-
-  ab2hex: function (buffer) {
-    var hexArr = Array.prototype.map.call(
-      new Uint8Array(buffer),
-      function (bit) {
-        return ('00' + bit.toString(16)).slice(-2)
-      }
-    )
-      return hexArr.join('');
-  },
-
-  /**
-   * 根据 uuid 获取处于已连接状态的设备
+   * 根据 uuid 获取手机系统已连接的蓝牙设备
    */
   getConnectedBluetoothDevices: function () {
     var that = this;
@@ -108,12 +73,12 @@ Page({
   },
 
   /**
-   * 建立蓝牙连接
+   * 建立BLE连接
    */
   createBLEConnection: function (e) {
     var that = this;
     wx.createBLEConnection({
-      // 这里的 deviceId 需要已经通过 createBLEConnection 与对应设备建立链接 
+      // 这里的 deviceId 由 getConnectedBluetoothDevices 获取
       deviceId: mDeviceID,
       success: function (res) {
         console.log(res)
@@ -140,7 +105,6 @@ Page({
   getBLEDeviceServices: function () {
     var that = this;
     wx.getBLEDeviceServices({
-      // 这里的 deviceId 需要已经通过 createBLEConnection 与对应设备建立链接
       deviceId: mDeviceID,
       success: function (res) {
         console.log('device services:', res.services)
@@ -163,9 +127,7 @@ Page({
   getBLEDeviceCharacteristics: function () {
     var that = this;
     wx.getBLEDeviceCharacteristics({
-      // 这里的 deviceId 需要已经通过 createBLEConnection 与对应设备建立链接
       deviceId: mDeviceID,
-      // 这里的 serviceId 需要在上面的 getBLEDeviceServices 接口中获取
       serviceId: SERVICE_UUID,
       success: function (res) {
         console.log('device getBLEDeviceCharacteristics:', res.characteristics)
@@ -182,18 +144,26 @@ Page({
     })
   },
 
+  ab2hex: function (buffer) {
+    var hexArr = Array.prototype.map.call(
+      new Uint8Array(buffer),
+      function (bit) {
+        return ('00' + bit.toString(16)).slice(-2)
+      }
+    )
+    return hexArr.join('');
+  },
+
   /**
-   * 启用低功耗蓝牙设备特征值变化时的 notify 功能，订阅特征值。注意：必须设备的特征值支持notify或者indicate才可以成功调用，具体参照characteristic 的 properties 属性
+   * 启用低功耗蓝牙设备特征值变化时的 notify 功能，订阅特征值。注意：必须设备的特征值支持notify或者
+   * indicate才可以成功调用，具体参照characteristic 的 properties 属性
    */
   notifyBLECharacteristicValueChange: function () {
     var that = this;
     wx.notifyBLECharacteristicValueChange({
       state: true, // 启用 notify 功能
-      // 这里的 deviceId 需要已经通过 createBLEConnection 与对应设备建立链接  
       deviceId: mDeviceID,
-      // 这里的 serviceId 需要在上面的 getBLEDeviceServices 接口中获取
       serviceId: SERVICE_UUID,
-      // 这里的 characteristicId 需要在上面的 getBLEDeviceCharacteristics 接口中获取
       characteristicId: NOTIFY_UUID,
       success: function (res) {
         console.log(res)
@@ -201,19 +171,54 @@ Page({
         wx.onBLECharacteristicValueChange(function (res) {
           console.log(`characteristic ${res.characteristicId} has changed, now is ${res.value}`)
           console.log(that.ab2hex(res.value))
-
-          wx.readBLECharacteristicValue({
-            // 这里的 deviceId 需要已经通过 createBLEConnection 与对应设备建立链接  [**new**]
-            deviceId: mDeviceID,
-            // 这里的 serviceId 需要在上面的 getBLEDeviceServices 接口中获取
-            serviceId: SERVICE_UUID,
-            // 这里的 characteristicId 需要在上面的 getBLEDeviceCharacteristics 接口中获取
-            characteristicId: NOTIFY_UUID,
-            success: function (res) {
-              console.log('readBLECharacteristicValue:', res.errCode)
-            }
-          })
         })
+      },
+      fail: function (res) {
+        console.log(res)
+        that.setData({
+          debugMsg: res.errMsg
+        })
+      },
+    })
+  },
+
+  /**
+   * 向蓝牙设备发送数据
+   */
+  writeBLECharacteristicValue: function () {
+    var that = this;
+    // 向蓝牙设备发送一个0x00的16进制数据
+    let buffer = new ArrayBuffer(1)
+    let dataView = new DataView(buffer)
+    dataView.setUint8(0, 0)
+
+    wx.writeBLECharacteristicValue({
+      deviceId: mDeviceID,
+      serviceId: SERVICE_UUID,
+      characteristicId: WRITE_UUID,
+      // 这里的value是ArrayBuffer类型
+      value: buffer,
+      success: function (res) {
+        console.log('writeBLECharacteristicValue success', res.errMsg)
+      },
+      fail: function (res) {
+        console.log(res)
+        that.setData({
+          debugMsg: res.errMsg
+        })
+      },
+    })
+  },
+
+  /**
+   * 搜索蓝牙设备
+   */
+  startBluetoothDevicesDiscovery: function (e) {
+    var that = this;
+    wx.startBluetoothDevicesDiscovery({
+      services: [SERVICE_UUID],
+      success: function (res) {
+        console.log(res)
       },
       fail: function (res) {
         console.log(res)
@@ -233,61 +238,6 @@ Page({
       success: function (res) {
         console.log(res)
       }
-    })
-  },
-
-  /**
-   * 写入数据
-   */
-  writeBLECharacteristicValue: function () {
-    var that = this;
-    // 向蓝牙设备发送一个0x00的16进制数据
-    let buffer = new ArrayBuffer(1)
-    let dataView = new DataView(buffer)
-    dataView.setUint8(0, 0)
-
-    wx.writeBLECharacteristicValue({
-      // 这里的 deviceId 需要在上面的 getBluetoothDevices 或 onBluetoothDeviceFound 接口中获取
-      deviceId: mDeviceID,
-      // 这里的 serviceId 需要在上面的 getBLEDeviceServices 接口中获取
-      serviceId: SERVICE_UUID,
-      // 这里的 characteristicId 需要在上面的 getBLEDeviceCharacteristics 接口中获取
-      characteristicId: WRITE_UUID,
-      // 这里的value是ArrayBuffer类型
-      value: buffer,
-      success: function (res) {
-        console.log('writeBLECharacteristicValue success', res.errMsg)
-      },
-      fail: function (res) {
-        console.log(res)
-        that.setData({
-          debugMsg: res.errMsg
-        })
-      },
-    })
-  },
-
-  /**
-   * 读取数据
-   */
-  readBLECharacteristicValue: function () {
-    var that = this;
-    wx.readBLECharacteristicValue({
-      // 这里的 deviceId 需要已经通过 createBLEConnection 与对应设备建立链接  [**new**]
-      deviceId: mDeviceID,
-      // 这里的 serviceId 需要在上面的 getBLEDeviceServices 接口中获取
-      serviceId: SERVICE_UUID,
-      // 这里的 characteristicId 需要在上面的 getBLEDeviceCharacteristics 接口中获取
-      characteristicId: NOTIFY_UUID,
-      success: function (res) {
-        console.log('readBLECharacteristicValue:', res.errCode)
-      },
-      fail: function (res) {
-        console.log(res)
-        that.setData({
-          debugMsg: res.errMsg
-        })
-      },
     })
   },
 })
